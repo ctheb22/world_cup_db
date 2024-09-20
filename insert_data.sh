85f4fc819003bfd6a2b1b7214a5739737cbeb570
Chance Beachy <chance.beachy@gmail.com>
2024-09-20 17:25:08 +0000

    Mid Project Backup

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,29 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+($PSQL "TRUNCATE teams, games")
+
+INSERT_TEAM () {
+  NAME=$1
+  TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$NAME'")
+  if [[ -z $TEAM_ID ]]
+  then
+    ADD_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$NAME')")
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$NAME'")
+  fi
+}
+
+HEADER=1
+cat games.csv | while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $HEADER == 1 ]]
+  then
+    (( HEADER-- ))
+  else
+    INSERT_TEAM $WINNER
+    WINNER_ID=$TEAM_ID 
+    INSERT_TEAM $OPPONENT
+    OPPONENT_ID=$TEAM_ID
+    RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
